532e7895d9eeebcb9d5485f3f38476a68bf5ea1d
heewookji <jhu2269@gmail.com>
2024-01-24 20:44:16 +0900

    feat: update invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -36,3 +36,24 @@ export async function createInvoice(formData: FormData) {
   // 다시 invoices로 돌아가기
   redirect('/dashboard/invoices');
 }
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
